3e6f80c8ab1328c7cbf1a5d662f692c4640c5023
thibault luyts <73276810+ThibaultLuytsPXL@users.noreply.github.com>
2023-03-29 10:38:26 +0200

    update Diceroller D20

---
+++ BenternetProject/diceroller.cpp
@@ -0,0 +1,72 @@
+#include "diceRoller.h"
+
+
+
+
+DiceRoller::DiceRoller(QCoreApplication *a)
+{
+    std::cout << "Prep!" << std::endl;
+    try
+    {
+    nzmqt::ZMQContext *context = nzmqt::createDefaultContext( a );
+    pusher = context->createSocket( nzmqt::ZMQSocket::TYP_PUSH, context );
+    subscriber = context->createSocket( nzmqt::ZMQSocket::TYP_SUB, context );
+    //QObject::connect( subscriber, &nzmqt::ZMQSocket::messageReceived, printMessages );
+    QObject::connect( subscriber, &nzmqt::ZMQSocket::messageReceived, this, &DiceRoller::ReciveMessage);
+    pusher->connectTo( "tcp://benternet.pxl-ea-ict.be:24041" );
+    subscriber->connectTo( "tcp://benternet.pxl-ea-ict.be:24042" );
+    if (!subscriber->isConnected() || !pusher->isConnected())
+        std::cout << "not connected\n" << std::endl;
+    subscriber->subscribeTo( "DiceRoller?" );
+
+
+
+
+
+    context->start();
+    }
+    catch( nzmqt::ZMQException & ex )
+    {
+        std::cerr << "Caught an exception : " << ex.what();
+    }
+    std::cout << "Start!" << std::endl;
+
+
+}
+
+void DiceRoller::ReciveMessage(const QList<QByteArray>& messages)
+{
+    for(QByteArray msgByteArray : messages) {
+        QString msg = QString::fromUtf8(msgByteArray);
+
+        if(msg == "DiceRoller?>6"){
+            DiceRoll();
+        }else if(msg == "DiceRoller?>20"){
+            DiceRoll20();
+        }else
+        {
+            //std::cout << "Wrong topic" << std::endl;
+            QString error = "DiceRoller!>Wrong topic";
+            nzmqt::ZMQMessage message = nzmqt::ZMQMessage( error.toUtf8() );
+            pusher->sendMessage( message );
+        }
+    }
+}
+
+void DiceRoller::DiceRoll()
+{
+    srand(time(0));
+    int dice = rand() %6 +1 ;
+    QString Dice = "DiceRoller!>" + QString::number(dice);
+    nzmqt::ZMQMessage message = nzmqt::ZMQMessage( Dice.toUtf8() );
+    pusher->sendMessage( message );
+}
+
+void DiceRoller::DiceRoll20()
+{
+    srand(time(0));
+    int dice = rand() %20 +1 ;
+    QString Dice = "DiceRoller!>" + QString::number(dice);
+    nzmqt::ZMQMessage message = nzmqt::ZMQMessage( Dice.toUtf8() );
+    pusher->sendMessage( message );
+}
